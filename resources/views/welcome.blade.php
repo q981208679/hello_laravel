4481350d9a732661515ee89d711e7a27cb0164bb
yuanyuan <981208679@qq.com>
2018-12-22 12:56:36 +0000

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Laravel</title>
+    <style type="text/css">
+        html,body{
+            height: 100%;
+        }
+        body{
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: "Helvetica Neue";
+            color: #777;
+        }
+        .container{
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+        .content{
+            text-align: center;
+            display: inline-block;
+
+        }
+        .title{
+            font-size: 66px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="title"> Hello Laravel!- by yuanyuan</div>
+        </div>
+    </div>
+</body>
+</html>
